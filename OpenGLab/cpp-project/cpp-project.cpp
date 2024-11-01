90dacba6b2bc8bddc6d02abb96a51d537650f00e
Muhammad Sulaiman <muhammad.b.sulaiman001@gmail.com>
2024-11-01 19:01:44 +0300

    spdlog and glm added to cpp-project

--- OpenGLab/cpp-project/cpp-project.cpp
+++ OpenGLab/cpp-project/cpp-project.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
+#include <glm/vec3.hpp>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    spdlog::info("Welcome to spdlog!");
+    auto vector3 = glm::vec3(.5, 1, 1);
+    std::cout << "vector3.x = " << vector3.x;
 }
